3bfd17e9cb27d90865bc840aa2208c1c5fe2a1e3
Bror Jonsson <brorfred@gmail.com>
2012-06-18 13:50:30 -0400

    Improved how the config file is read. moved a bunch of defaults to the cfg file. Removed some dependancies.

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+"""Setup file to generate a distribution of projmap
+
+use as python setup.py sdist"""
+
+
 from distutils.core import setup
+
 setup(name='projmap',
       version='0.5',
       description='Higher level version of matplotlibs Basemap',
@@ -7,6 +13,6 @@ setup(name='projmap',
       url='http://brorfred.org/python_dists/projmap/',
       requires=["numpy(>=1.5)", "matplotlib(>=1.1.0)"],
       packages=['projmap'],
-      package_data={'projmap': ['data/*.txt','map_regions.cfg']},
+      package_data={'projmap': ['data/*.txt', 'map_regions.cfg']},
       #data_files=[('projmap', ['projmap/map_regions.cfg',])]
      )
