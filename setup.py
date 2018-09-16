1480c067c565e6d879fc1639e971dcd98d515f2f
Bror Jonsson <brorfred@gmail.com>
2018-09-16 09:08:32 -0400

    Incerase version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ use as python setup.py sdist"""
 from setuptools import setup, find_packages
 
 setup(name='projmap',
-      version='0.7.2',
+      version='0.8',
       description='High level wrapper of matplotlib-Basemap',
       author='Bror Jonsson',
       author_email='brorfred@gmail.com',
