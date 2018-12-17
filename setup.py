2a8ca3522b25949aae44753acc62483c53add5d8
Bror Jonsson <brorfred@gmail.com>
2018-12-17 13:34:35 +0000

    Fix prerequisites

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ from setuptools import setup, find_packages
 
 setup(name='projmap',
       version='0.81',
-      description='High level wrapper of matplotlib-Basemap',
+      description='High level wrapper of matplotlibs mapping package',
       author='Bror Jonsson',
       author_email='brorfred@gmail.com',
       url='https://github.com/brorfred/projmap',
-      requires=["numpy(>=1.5)", "matplotlib(>=1.1.0)", "six", "basemap"],
+      requires=["numpy(>=1.5)", "matplotlib(>=2.0)", "six",
+                "cartopy(>=0.16)"],
       packages=['projmap'],
       package_data={'projmap': ['data/*.txt', 'map_regions.cfg']},
      )
